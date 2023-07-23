0072a17f557c1481d765f5961feb0069bbd1ef3e
Franck Hu <byfranckhu@gmail.com>
2023-07-23 23:35:40 +0200

    style(rgb): style rgb color picker

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
   import RGBPicker from './component/RGBPicker.svelte';
   import { color } from './store/color';
+  import { darkTheme } from './store/theme';
+  import type { Color } from './type/color';
 
+  function updateTheme(color: Color) {
+    return color.red <= 127 || color.green <= 127 || color.blue <= 127;
+  }
+
+  $: $darkTheme = updateTheme($color);
   $: rgbstring = `rgb(${$color.red}, ${$color.green}, ${$color.blue})`;
 </script>
 
-<main style="--color: {rgbstring}">
+<main class:dark={$darkTheme} style="--color: {rgbstring}">
   <div class="container">
     <RGBPicker />
   </div>
@@ -13,12 +20,15 @@
 
 <style>
   main {
-    width: 100vw;
     height: 100vh;
     background: var(--color);
   }
 
   .container {
     padding: 16px;
+  }
+
+  .dark {
+    color: white;
   }
 </style>
--- src/type/color.d.ts
+++ src/type/color.d.ts
@@ -1,4 +1,4 @@
-interface Color {
+export interface Color {
   red: number;
   green: number;
   blue: number;
