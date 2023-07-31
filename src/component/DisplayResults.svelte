359dc6bbde57be6466a428fae548f362babe0fd7
Franck Hu <byfranckhu@gmail.com>
2023-07-31 21:22:21 +0200

    style: design copy button

--- src/component/DisplayResults.svelte
+++ src/component/DisplayResults.svelte
@@ -4,19 +4,30 @@
 </script>
 
 <div class="container">
-  <div class="result">{$color.toRgb()}</div>
+  <div class="result">
+    <div>RGB</div>
+    <div>{$color.toRgb()}</div>
+  </div>
   <button>
     <img src="/copy.svg" alt="copy icon" />
   </button>
 </div>
 <div class="container">
-  <div class="result">{$color.toHex()}</div>
+  <div class="result">
+    <div>HEX</div>
+    <div>
+      {$color.toHex()}
+    </div>
+  </div>
   <button>
     <img src="/copy.svg" alt="copy icon" />
   </button>
 </div>
 <div class="container">
-  <div class="result">{Hsl.fromColor($color).toString()}</div>
+  <div class="result">
+    <div>HSL</div>
+    <div>{Hsl.fromColor($color).toString()}</div>
+  </div>
   <button>
     <img src="/copy.svg" alt="copy icon" />
   </button>
@@ -31,16 +42,23 @@
   }
 
   .result {
-    padding: 16px 0 16px 16px;
+    display: flex;
+    gap: 128px;
+    padding-inline-start: 16px;
   }
 
   button {
     border: none;
     background: none;
     cursor: pointer;
+    padding: 16px;
   }
 
   button:hover {
-    background: red;
+    background: rgb(0, 0, 0, 0.1);
+  }
+
+  img {
+    filter: brightness(0) invert(1);
   }
 </style>
