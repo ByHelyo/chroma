6a983e0dfc3abe5beacb0d07a651e03576b033c3
Franck Hu <byfranckhu@gmail.com>
2023-08-01 00:18:10 +0200

    refacto: refacto DisplayResults

---
+++ src/component/CopyButton.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { writeText } from '@tauri-apps/api/clipboard';
+
+  export let text;
+</script>
+
+<button on:click={() => writeText(text)}>
+  <div>COPY</div>
+  <img src="/copy.svg" alt="copy icon" />
+</button>
+
+<style>
+  button {
+    border: none;
+    background: none;
+    cursor: pointer;
+    padding: 16px;
+    display: flex;
+    align-items: center;
+    color: white;
+    font-size: 16px;
+    font-weight: bold;
+    gap: 16px;
+  }
+
+  button:hover {
+    background: rgb(0, 0, 0, 0.1);
+  }
+
+  button:active {
+    background: rgb(0, 0, 0, 0.3);
+  }
+
+  img {
+    filter: brightness(0) invert(1);
+  }
+</style>
---
+++ src/component/DisplayResult.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import CopyButton from './CopyButton.svelte';
+
+  export let label: string;
+
+  export let result: string;
+</script>
+
+<div class="container">
+  <div class="result">
+    <div>{label}</div>
+    <div>{result}</div>
+  </div>
+  <CopyButton text={result} />
+</div>
+
+<style>
+  .container {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background: rgb(127, 127, 127, 0.4);
+  }
+
+  .result {
+    display: flex;
+    gap: 128px;
+    padding-inline-start: 16px;
+  }
+</style>
